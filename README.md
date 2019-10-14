f551644b57fc52fa36e372e9239a1efa6e8c5ca3
trogyuo <hit_troy@126.com>
2019-10-14 23:22:55 +1400

    1. Add 2019 downloading, for usa 2019 is open 2. Add some exceptions handling to be more robust

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # dvcon_download 
-Download proccedings from DVCon
+Download proceedings from DVCon USA/ DVCon Euorpe
 
-##Overview 
-This script is intented to download the DVCON papers from the offical website. For the url has some common format, downloading them by hand would be rather tedious. Here comes an idea to write a simple script to download them.
+## Overview 
+This script is intended to download the DVCON papers from the official website. For the url has some common format, downloading them by hand would be rather tedious. Here comes an idea to write a simple script to download them.
 
 
 ## How to use 
-```
+```python
 git clone https://github.com/troyguo/dvcon_download.git
 
 python3 dvcon_dowload/download.py
@@ -14,5 +14,15 @@ python3 dvcon_dowload/download.py
 After running, folder named by location and year will be seen under the current folder, for example, USA_2018
 
 
-## Limtation 
-Currently, for DVCON Europe, only the year afer 2016 can be downloaded, it's caused by the url format diffrence from the year 2015 and 2016.
+## Limitation 
+Currently, for DVCON Europe, only the year after 2016 can be downloaded, it's caused by the url format difference from the year 2015 and 2016.
+
+
+
+
+
+## TODO:
+
+1. Add dvcon India/China downloading
+2. compatible with DVCon Europe 2015 format.
+
--- download.py
+++ download.py
@@ -1,5 +1,6 @@
 import urllib3
 import os
+import logging
 
 #"http://events.dvcon.org/Europe/2018/proceedings/papers/01_3.pdf"
 temp_str = "http://events.dvcon.org/{location}/{year:0d}/proceedings/{type_s}/{chapter:02d}_{item:0d}.pdf"
@@ -11,9 +12,10 @@ def download_file(connection, type_s, download_url):
     ##try to connect to the website, if not success, skip the writing file step.
     rsp = connection.request("GET", download_url)
     if (rsp.status != 200):
+        #logging.info("Failed to Download from {}".format(download_url))
         return  False
 
-    print("Downloading from {}".format(download_url))
+    logging.info("Successfully Downloading from {}".format(download_url))
 
     ##write to file, the name is the basename, such as 10_1.pdf
     file_name = type_s + "_" + os.path.basename(download_url)
@@ -27,18 +29,27 @@ def download_file(connection, type_s, download_url):
 
 def main(connection):
     locations = ["USA", "Europe"]
-    years = [2018, 2017, 2016]
+    years = [2019] 2018, 2017, 2016]
     cur_dir = os.path.abspath(os.path.curdir)
 
     ##TODO: split this function into smaller ones.
     for location in locations:
         for year in years:
             rel_path = location + "_{year:0d}".format(year = year)
-            ##create a subfolder, format: Europe_2018, US
+            ##create a subfolder, format: Europe_2018, USA_2018
             dir_name = os.path.join(cur_dir, rel_path)
-            os.mkdir(dir_name)
+            try:
+                os.mkdir(dir_name)
+            except FileExistsError:
+                s = "Folder {dir} already exists, skipping".format(dir = dir_name)
+                logging.warning(s)
+                continue
+
             ##
-            os.chdir(dir_name)
+            try:
+                os.chdir(dir_name)
+            except:
+                sys.exit("cannot chdir to dir {}".format(dir_name))
 
             ##if USA, url for location should be empty
             if(location == "USA"):
@@ -57,11 +68,15 @@ def main(connection):
                             if(not ret):
                                 break
 
-            ##change back.
-            os.chdir(cur_dir)
+            ##chdir back.
+            try:
+                os.chdir(cur_dir)
+            except:
+                sys.exit("cannot chdir to dir {}".format(dir_name))
             ##
 
 if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.INFO)
     connection = urllib3.PoolManager()
     main(connection)
     connection.clear()
